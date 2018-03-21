1c4523d5f4ce67ca86b1578e0ccb3909354d3cc3
Blake Watkins <iamsoblake@gmail.com>
2018-03-21 14:32:44 -0700

    working skill

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -10,17 +10,13 @@
 
 'use strict';
 const Alexa = require('alexa-sdk');
-
-const dotenv = require('dotenv');
-dotenv.load();
-
+const ENV = require('./env.js');
 const OAuth = require('oauth');
-
-
-const CONSUMER_KEY = process.env.CONSUMER_KEY;
-const CONSUMER_SECRET = process.env.CONSUMER_SECRET;
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const CONSUMER_KEY = ENV.twitter.consumer.public;
+const CONSUMER_SECRET = ENV.twitter.consumer.secret;
+const ACCESS_TOKEN = ENV.twitter.user.token;
+const ACCESS_TOKEN_SECRET = ENV.twitter.user.secret;
+const APP_ID = ENV.twitter.alexaSkillId;
 
 const oauth = new OAuth.OAuth(
   'https://api.twitter.com/oauth/request_token',
@@ -31,109 +27,92 @@ const oauth = new OAuth.OAuth(
   null,
   'HMAC-SHA1'
 );
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
 
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
-//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
 const PARAMS = {
   q: 'realoverheardla',
+  until: `${datestring()}`,
   result_type: 'mixed',
   count: 100
 }
 
 const LEN = Object.keys(PARAMS).length;
-const URLSUFFIX = Object.keys(PARAMS).reduce(( acc, curr, idx) => {
+const URLSUFFIX = Object.keys(PARAMS).reduce((acc, curr, idx) => {
   let suffix = idx === LEN - 1 ? '' : '&';
   acc += `${curr}=${PARAMS[curr]}${suffix}`;
   return acc;
 }, '')
 
-
-const APP_ID = undefined;
-
-const SKILL_NAME = 'Ya Heard';
-const GET_HEARD_MESSAGE = "Here's a random blurb from some smuck in Los Angeles: ";
-const HELP_MESSAGE = 'You can say tell me quote, or, you can say exit... What can I help you with?';
-const HELP_REPROMPT = 'What can I help you with?';
-const STOP_MESSAGE = 'Goodbye!';
-
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/lambda/data
-//=========================================================================================================================================
-// const data = [
-//     'A year on Mercury is just 88 days long.',
-//     'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-//     'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-//     'On Mars, the Sun appears about half the size as it does on Earth.',
-//     'Earth is the only planet not named after a god.',
-//     'Jupiter has the shortest day of all the planets.',
-//     'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-//     'The Sun contains 99.86% of the mass in the Solar System.',
-//     'The Sun is an almost perfect sphere.',
-//     'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-//     'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-//     'The temperature inside the Sun can reach 15 million degrees Celsius.',
-//     'The Moon is moving approximately 3.8 cm away from our planet every year.'
-// ];
-
-const data = await oauth.get(
-  `https://api.twitter.com/1.1/search/tweets.json?${URLsUFFIX}`,
-  ACCESS_TOKEN,
-  ACCESS_TOKEN_SECRET,
-  function (error, result, response){
-    if (error) console.error(error);
-
-    return JSON.parse(result).statuses.map(datum => {
-      return datum.text;
-    })
-    .filter(datum => {
-      return datum.indexOf('//') < 0 && datum.length > 75;
-    })
-    .map(datum => datum.replace(/(\r\n\t|\n|\r\t)/gm,""))
-    .map(datum => datum.replace(/"/g, ''))
-
-});
-
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.
-//=========================================================================================================================================
-
-exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.appId = APP_ID;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+function datestring() {
+  var d = new Date();
+  return d.getUTCFullYear()   + '-'
+     +  (d.getUTCMonth() + 1) + '-'
+     +  (d.getDate() - Math.floor(Math.random() * 7));
 };
 
+const SKILL_NAME = 'Local Smut';
+const GET_TWITHEARD_MESSAGE = "Here's a random smut from some smuck in Los Angeles: ";
+const HELP_MESSAGE = 'You can say tell me some smut, or, you can say gtfo... What can I even help you with?';
+const HELP_REPROMPT = 'What the heck can I even help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+
+let twitData = new Promise((resolve, reject) => {
+  oauth.get(
+    `https://api.twitter.com/1.1/search/tweets.json?${URLSUFFIX}`,
+    ACCESS_TOKEN,
+    ACCESS_TOKEN_SECRET,
+    function (error, result, response){
+      if (error) return reject(error);
+
+      return resolve( JSON.parse(result).statuses.map(datum => {
+        return datum.text;
+      })
+        .filter(datum => {
+          return datum.indexOf('//') < 0 && datum.length > 75;
+        })
+        .map(datum => datum.replace(/(\r\n\t|\n|\r\t)/gm,""))
+        .map(datum => datum.replace(/"/g, ''))
+      )
+    });
+})
+
 const handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetNewFactIntent');
-    },
-    'GetNewFactIntent': function () {
-        const heardArr = data;
-        const heardIndex = Math.floor(Math.random() * heardArr.length);
-        const randomHeard = heardArr[heardIndex];
-        const speechOutput = GET_HEARD_MESSAGE + randomHeard;
+  'LaunchRequest': function () {
+    this.emit('GetNewFactIntent');
+  },
+  'GetNewFactIntent': function () {
+    let self = this;
+    twitData
+      .then(twitheardArr => {
+        const twitheardIndex = Math.floor(Math.random() * twitheardArr.length);
+        const randomTwitheard = twitheardArr[twitheardIndex];
+        const speechOutput = GET_TWITHEARD_MESSAGE + randomTwitheard;
 
-        this.response.cardRenderer(SKILL_NAME, randomHeard);
-        this.response.speak(speechOutput);
-        this.emit(':responseReady');
-    },
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = HELP_MESSAGE;
-        const reprompt = HELP_REPROMPT;
+        self.response.cardRenderer(SKILL_NAME, randomTwitheard);
+        self.response.speak(speechOutput);
+        self.emit(':responseReady');
+      });
+  },
+  'AMAZON.HelpIntent': function () {
+    const speechOutput = HELP_MESSAGE;
+    const reprompt = HELP_REPROMPT;
 
-        this.response.speak(speechOutput).listen(reprompt);
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent': function () {
-        this.response.speak(STOP_MESSAGE);
-        this.emit(':responseReady');
-    },
-    'AMAZON.StopIntent': function () {
-        this.response.speak(STOP_MESSAGE);
-        this.emit(':responseReady');
-    },
+    this.response.speak(speechOutput).listen(reprompt);
+    this.emit(':responseReady');
+  },
+  'AMAZON.CancelIntent': function () {
+    this.response.speak(STOP_MESSAGE);
+    this.emit(':responseReady');
+  },
+  'AMAZON.StopIntent': function () {
+    this.response.speak(STOP_MESSAGE);
+    this.emit(':responseReady');
+  },
+};
+
+exports.handler = function(event, context, callback) {
+  let alexa = Alexa.handler(event, context);
+  alexa.appId = APP_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
